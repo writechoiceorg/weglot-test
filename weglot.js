98cbb2596bfdfe87f7689d21ed63c0309a7c8ee5
Arcapas <dfurfuro@gmail.com>
2025-09-30 14:03:53 -0300

    update

--- weglot.js
+++ weglot.js
@@ -1,6 +1,7 @@
 Weglot.initialize({
        api_key: 'wg_74ea5f282828370b0c827c288f63f15e2',
-       auto_switch: true,
-       respect_do_not_track: false,
-       cache: false
-   });    
+       excluded_elements: ['title']
+   }); 
+  
+
+   
